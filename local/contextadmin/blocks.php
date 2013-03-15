3a4db817d6b311592992a2b32ad5ad6a6986fd09
Trevor Jones <tdjones@ualberta.ca>
2013-03-15 15:02:23 -0600

    eclass-local-contextadmin
    Blocks are now able to be hidden/shown,overriden, and locked via the UI at the category level.

--- local/contextadmin/blocks.php
+++ local/contextadmin/blocks.php
@@ -1,26 +1,48 @@
 <?php
-
-// Allows the category manager to configure blocks (hide/show)
+// Allows the admin to manage activity modules
+global $CFG;
+global $PAGE;
+global $OUTPUT;
+global $DB;
 require_once(dirname(__FILE__) . '/../../config.php');
-require_once($CFG->libdir.'/adminlib.php');
+require_once($CFG->dirroot.'/course/lib.php');
 require_once($CFG->dirroot.'/local/contextadmin/locallib.php');
+require_once($CFG->libdir.'/adminlib.php');
 require_once($CFG->libdir.'/tablelib.php');
+
+
+// defines
+define('MODULE_TABLE','module_administration_table');
+
+//admin_externalpage_setup('managemodules');
+
 
 require_login();
 
 $contextid = required_param("contextid", PARAM_INT);
 $catid = required_param("catid",PARAM_INT);
-$confirm  = optional_param('confirm', 0, PARAM_BOOL);
-$hide     = optional_param('hide', 0, PARAM_INT);
-$show     = optional_param('show', 0, PARAM_INT);
 
-// Get the context that was passed (verify it is in a context beneath or equal to category
+// Get the context that was passed (verify it is course category or system context).
+//$context = get_context_instance(CONTEXT_COURSECAT,$catid);
 $context = get_context_instance_by_id($contextid, MUST_EXIST);
-//TODO: Exit cleanly...fix this later, check valid context
+
+
+//TODO: Exit cleanly...fix this later
+// If we do not belong here....
+
 
 // Setup Page (not admin setup)
 $PAGE->set_url("/local/contextadmin/blocks.php", array("contextid" => $contextid, "catid" => $catid));
 $PAGE->set_category_by_id($catid);
+//$PAGE->set_context($context);
+
+$param_visible  = optional_param('visible', null, PARAM_BOOL);
+$param_clear    = optional_param('clear',null, PARAM_BOOL);
+$param_delete   = optional_param('delete', null, PARAM_BOOL);
+$param_confirm  = optional_param('confirm', null, PARAM_BOOL);
+$param_override = optional_param('override', null, PARAM_BOOL);
+$param_locked   = optional_param('locked', null, PARAM_BOOL);
+$param_block_name = optional_param('block_name', '',PARAM_SAFEDIR);
 
 /// Print headings
 $strmanageblocks = get_string('manageblocks');
@@ -29,33 +51,52 @@ $strshow = get_string('show');
 $strsettings = get_string('settings');
 $strname = get_string('name');
 $strshowblockcourse = get_string('showblockcourse');
+$strclear_heading = get_string('clear_title','local_contextadmin');
+$stroverride_heading = get_string('override_title','local_contextadmin');
+$strlocked_heading = get_string('locked_title','local_contextadmin');
+$strsettings = get_string("settings");
+$strblocks = get_string("blocks");
+$stractivitymodule = get_string("activitymodule");
+$strshowmodulecourse = get_string('showmodulecourse');
+
 
 /// If data submitted, then process and store.
-if (!empty($hide) && confirm_sesskey()) {
-    if (!$block = $DB->get_record('block', array('id'=>$hide))) {
-        print_error('blockdoesnotexist', 'error');
+if ((!empty($param_block_name)) and confirm_sesskey() && has_capability('mod/contextadmin:changevisibilty', $context)) {
+
+    $module = $DB->get_record("block", array("name"=>$param_block_name));
+
+    if (!$module) {
+        print_error('noblocks', 'error');
     }
     else {
-        set_context_block_settings($catid,$block->name,array('visible'=>'0'));
+        if (!is_plugin_locked($catid,$param_block_name,'block')) {
+            if ($param_visible !== null) {
+
+                set_context_block_settings($catid, $param_block_name, array('visible' => $param_visible, 'search' => ''));
+            }
+            if ($param_override !== null) {
+                set_context_block_settings($catid, $param_block_name, array('override' => $param_override, 'search' => ''));
+            }
+            if ($param_locked !== null) {
+                set_context_block_settings($catid, $param_block_name, array('locked' => $param_locked, 'search' => ''));
+            }
+            if($param_clear !== null){
+                remove_category_block_values($catid,$param_block_name);
+            }
+        }
+        else {
+            print_error('blocklocked', 'local_contextadmin');
+        }
     }
 }
 
-if (!empty($show) && confirm_sesskey() ) {
-    if (!$block = $DB->get_record('block', array('id'=>$show))) {
-        print_error('blockdoesnotexist', 'error');
-    }
-    else {
-        set_context_block_settings($catid,$block->name,array('visible'=>'1'));
-    }
-}
-
+// Category is our primary source of context.  This is important.
 // Setup the PAGE object
 $category = $PAGE->category;
 $site = get_site();
 $PAGE->set_title("$site->shortname: $category->name");
 $PAGE->set_heading($site->fullname);
 $PAGE->set_pagelayout('coursecategory');
-
 echo $OUTPUT->header();
 echo $OUTPUT->heading($category->name.': '.$strmanageblocks);
 
@@ -71,17 +112,17 @@ $incompatible = array();
 $table = new flexible_table('admin-blocks-compatible');
 //TODO: tying the capability to hide/show blocks to the same one for hide/show modules. Might need it's own in the future.
 if(has_capability('mod/contextadmin:editowncatsettings', $context)) {
-    $table->define_columns(array('name', 'hideshow', 'settings'));
-    $table->define_headers(array($strname, $strhide.'/'.$strshow, $strsettings));
+    $table->define_columns(array('name', 'hideshow', 'override','lock','clear'));
+    $table->define_headers(array($stractivitymodule, "$strhide/$strshow", $stroverride_heading, $strlocked_heading, $strclear_heading));
 }
 // User can not edit settings for modules but can hide/show
 else if(has_capability('mod/contextadmin:changevisibilty', $context)){
-    $table->define_columns(array('name', 'hideshow'));
-    $table->define_headers(array($strname, $strhide.'/'.$strshow));
+    $table->define_columns(array('name', 'hideshow','clear','override','lock','clear'));
+    $table->define_headers(array($stractivitymodule, "$strhide/$strshow", $stroverride_heading, $strlocked_heading, $strclear_heading));
 }
 else {
     $table->define_columns(array('name'));
-    $table->define_headers(array($strname));
+    $table->define_columns(array($stractivitymodule));
 }
 
 $table->define_baseurl($CFG->wwwroot.'/'.$CFG->admin.'/blocks.php');
@@ -90,6 +131,14 @@ $table->setup();
 $tablerows = array();
 
 foreach ($blocks as $blockid=>$blockraw) {
+    $visible_td ='';
+    $clear_td = '';
+    $override_td = '';
+    $locked_td = '';
+    $settings_td = '';
+
+
+
     // Get current block settings (hidden/shown, etc..) based off category table cascaded.
     $block = get_context_block_settings($catid,$blockraw->name);
     $blockname = $block->name;
@@ -112,7 +161,7 @@ foreach ($blocks as $blockid=>$blockraw) {
         $strblockname = get_string('pluginname', 'block_'.$blockname);
     }
 
-    $settings = ''; // By default, no configuration
+
     //TODO: add this later for site admins  or those with the capability to edit settings
 //    if ($blockobject and $blockobject->has_config()) {
 //        $blocksettings = admin_get_root()->locate('blocksetting' . $block->name);
@@ -141,32 +190,48 @@ foreach ($blocks as $blockid=>$blockraw) {
 //    }
     $class = ''; // Nothing fancy, by default
 
-    if (!$blockobject || !has_capability('mod/contextadmin:changevisibilty', $context)) {
-        // ignore
-        $visible = '';
-    } else if ($block->visible) {
-        $visible = '<a href="blocks.php?contextid='.$contextid.'&catid='.$catid.'&hide='.$blockid.'&amp;sesskey='.sesskey().'" title="'.$strhide.'">'.
-            '<img src="'.$OUTPUT->pix_url('i/hide') . '" class="icon" alt="'.$strhide.'" /></a>';
-    } else {
-        $visible = '<a href="blocks.php?contextid='.$contextid.'&catid='.$catid.'&show='.$blockid.'&amp;sesskey='.sesskey().'" title="'.$strshow.'">'.
-            '<img src="'.$OUTPUT->pix_url('i/show') . '" class="icon" alt="'.$strshow.'" /></a>';
-        $class = ' class="dimmed_text"'; // Leading space required!
+
+    if(has_capability('mod/contextadmin:changevisibilty', $context)){
+        $self_path = "blocks.php?contextid=$contextid&catid=$catid";
+
+        if (!$blockobject) {
+            // ignore
+            $visible_td = '';
+        } else if ($block->visible) {
+            $visible_td = create_form($OUTPUT,$blockname.'_visible_form',$self_path,$strhide,'hide',array('block_name'=>$blockname, 'visible'=>'false', 'sesskey'=>sesskey()));
+        } else {
+            $visible_td = create_form($OUTPUT,$blockname.'_visible_form',$self_path,$strshow,'show',array('block_name'=>$blockname, 'visible'=>'true', 'sesskey'=>sesskey()));
+            $class = ' class="dimmed_text"'; // Leading space required!
+        }
+
+        if(category_block_exists($catid,$blockname)){
+            $clear_td = create_form($OUTPUT,$blockname."_clear_form",$self_path,$strhide,'cross_red_big',array('block_name'=>$blockname, 'clear'=>'true', 'sesskey'=>sesskey()));
+
+            if ($block->override) {
+                $override_td = create_form($OUTPUT,$blockname."_override_form",$self_path,$strhide,'completion-manual-y',array('block_name'=>$blockname, 'override'=>'false', 'sesskey'=>sesskey()));
+                $class   = '';
+            } else {
+                $override_td = create_form($OUTPUT,$blockname."_override_form",$self_path,$strhide,'completion-manual-n',array('block_name'=>$blockname, 'override'=>'true', 'sesskey'=>sesskey()));
+            }
+
+            if ($block->locked) {
+                $locked_td = create_form($OUTPUT,$blockname."_locked_form",$self_path,$strhide,'completion-manual-y',array('block_name'=>$blockname, 'locked'=>'false', 'sesskey'=>sesskey()));
+                $class   = '';
+            } else {
+                $locked_td = create_form($OUTPUT,$blockname."_locked_form",$self_path,$strhide,'completion-manual-n',array('block_name'=>$blockname, 'locked'=>'true', 'sesskey'=>sesskey()));
+            }
+
+        }
+
     }
 
-    $row =  array('<span'.$class.'>'.$strblockname.'</span>');
-    if(!empty($visible)) {
-        $row[] = $visible;
-    }
-    if(!empty($settings)) {
-        $row[] = $settings;
-    }
+    $tabledata = array('<span'.$class.'>'.$blockname.'</span>');
+    $tabledata[] = $visible_td;
+    $tabledata[] = $override_td;
+    $tabledata[] = $locked_td;
+    $tabledata[] = $clear_td;
 
-    $tablerows[] = array(strip_tags($strblockname), $row); // first element will be used for sorting
-}
-
-collatorlib::asort($tablerows);
-foreach ($tablerows as $row) {
-    $table->add_data($row[1]);
+    $table->add_data($tabledata);
 }
 
 //TODO: Deprecated function
